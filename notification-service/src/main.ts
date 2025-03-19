45147b3efcfcc626814acc44bbb1eafbd511acd8
Denis Steckij <steckijdenis12@gmail.com>
2025-03-19 22:03:53 +0300

    connect notification

--- notification-service/src/main.ts
+++ notification-service/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.connectMicroservice({
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://localhost:5672'],
+      queue: 'notification_queue',
+      queueOptions: {
+        durable: false,
+      },
+    },
+  });
+  await app.startAllMicroservices();
+  await app.listen(3002);
+  console.log('notification service was started');
 }
 bootstrap();
--- user-service/src/user/user.controller.ts
+++ user-service/src/user/user.controller.ts
@@ -2,10 +2,27 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CatchError } from 'src/common/error.decorator';
 import { createUserDto } from './dto/create_user.dto';
+import {
+  ClientProxy,
+  ClientProxyFactory,
+  Transport,
+} from '@nestjs/microservices';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  private notificationClient: ClientProxy;
+  constructor(private readonly userService: UserService) {
+    this.notificationClient = ClientProxyFactory.create({
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://localhost:5672'],
+        queue: 'notification_queue',
+        queueOptions: {
+          durable: false,
+        },
+      },
+    });
+  }
 
   @Get()
   @CatchError('probem witn getting user')
@@ -18,7 +35,19 @@ export class UserController {
   async create(@Body() createUserDto: createUserDto) {
     const { email, password } = createUserDto;
     const user = await this.userService.create(email, password);
-
+    this.notificationClient
+      .emit('user_created', {
+        id: user.id,
+        email: user.email,
+      })
+      .subscribe({
+        next: () => {
+          console.log('Notification sent:');
+        },
+        error: (error) => {
+          console.error('Error sending notification:', error);
+        },
+      });
     return user;
   }
 }
